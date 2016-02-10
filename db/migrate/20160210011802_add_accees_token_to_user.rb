db891374fbe39830f418c9ae099d4131136355fb
Shia <rise.shia@gmail.com>
2016-02-10 10:19:17 +0900

    add access_token to users

---
+++ db/migrate/20160210011802_add_accees_token_to_user.rb
@@ -0,0 +1,5 @@
+class AddAcceesTokenToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :access_token, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160110054920) do
+ActiveRecord::Schema.define(version: 20160210011802) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20160110054920) do
     t.datetime "updated_at",                          null: false
     t.string   "provider"
     t.string   "uid"
+    t.string   "access_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
