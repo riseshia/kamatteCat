1ff6c111d41b19528f50fcbcac8edd2bf768a655
Shia <rise.shia@gmail.com>
2016-02-13 00:51:45 +0900

    fix the test

--- spec/views/repositories/index.html.erb_spec.rb
+++ spec/views/repositories/index.html.erb_spec.rb
@@ -8,14 +8,14 @@ RSpec.describe "repositories/index", type: :view do
         :full_name => "Full Name",
         :html_url => "Html Url",
         :github_id => 1,
-        :is_target => 2
+        :is_target => 0
       ),
       Repository.create!(
         :name => "Name",
         :full_name => "Full Name",
         :html_url => "Html Url",
         :github_id => 1,
-        :is_target => 2
+        :is_target => 0
       )
     ])
   end
@@ -25,6 +25,6 @@ RSpec.describe "repositories/index", type: :view do
     assert_select "tr>td", :text => "Name".to_s, :count => 2
     assert_select "tr>td", :text => "Full Name".to_s, :count => 2
     assert_select "tr>td", :text => "Go to github".to_s, :count => 2
-    assert_select "tr>td", :text => 2.to_s, :count => 2
+    assert_select "tr>td", :text => "OFF".to_s, :count => 2
   end
 end
