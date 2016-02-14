785eca0eed476f3b576e7cd7dbf97ade01ed1ee2
Shia <rise.shia@gmail.com>
2016-02-14 19:24:28 +0900

    limit search target to only users commit

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -3,7 +3,7 @@ class ReportMailer < ApplicationMailer
     client = user.client
 
     @list = user.repos_to_check.reduce({}) do |hash, r|
-      commits = client.commits(r.full_name, since: (Time.now - 14.days).utc.iso8601, until: (Time.now - 13.days).utc.iso8601)
+      commits = client.commits(r.full_name, author: user.email, since: (Time.now - 14.days).utc.iso8601, until: (Time.now - 13.days).utc.iso8601)
       hash[r.full_name] = commits unless commits.size == 0
       hash
     end
