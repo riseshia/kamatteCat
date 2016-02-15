91dcb634d346039b51d9ecf2f74f1cfb173f3e98
Shia <rise.shia@gmail.com>
2016-02-15 10:01:33 +0900

    smtp domain setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => 'smtp.gmail.com', :port => 587,
+    :domain => 'kcat.nyatorie.com',
     :authentication => :plain,
     :enable_starttls_auto => true,
     :user_name => ENV['MAIL_ACCOUNT'],
