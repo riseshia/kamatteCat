482b357465b84848d45c3874a73c57c631dd9794
Shia <rise.shia@gmail.com>
2016-02-15 09:50:33 +0900

    mail will not be sent user had any commit before 2 weeks age

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -2,12 +2,16 @@ class ReportMailer < ApplicationMailer
   def commits_on_2weeks_ago(user)
     client = user.client
 
+    count = 0
     @list = user.repos_to_check.reduce({}) do |hash, r|
       commits = client.commits(r.full_name, author: user.email, since: (Time.now - 14.days).utc.iso8601, until: (Time.now - 13.days).utc.iso8601)
-      hash[r.full_name] = commits unless commits.size == 0
+      unless commits.size == 0
+        hash[r.full_name] = commits 
+        count += 1
+      end
       hash
     end
 
-    mail(to: user.email, subject: "[#{Time.zone.now.strftime('%F')}] Your commits before 2 weeks ago.")
+    mail(to: user.email, subject: "[#{Time.zone.now.strftime('%F')}] Your commits before 2 weeks ago.") unless count == 0
   end
 end
